06195b465ec07045af12cc18df8044bb283bd62a
Omar Mendo <omendo@uoc.edu>
2024-05-26 18:43:28 +0100

    Config dbt and add dbt model for locations

---
+++ dbt/gentrification/models/locations_processed.sql
@@ -0,0 +1,38 @@
+-- models/locations_processed.sql
+
+{{ config(materialized='table') }}
+
+with date_ranges as (
+    select
+        nombre,
+        actividad_tipo,
+        municipio_codigo,
+        municipio_nombre,
+        longitud,
+        latitud,
+        generate_series(
+            date_trunc('year', to_date(fecha_creacion, 'YYYY-MM-DD')),
+            date_trunc('year', to_date(fecha_actualizacion, 'YYYY-MM-DD')),
+            interval '1 year'
+        ) as year
+    from (
+        select nombre, actividad_tipo, municipio_codigo, municipio_nombre, longitud, latitud, fecha_creacion, fecha_actualizacion from public.locations_commerce
+        union all
+        select nombre, actividad_tipo, municipio_codigo, municipio_nombre, longitud, latitud, fecha_creacion, fecha_actualizacion from public.locations_hostelry
+        union all
+        select nombre, actividad_tipo, municipio_codigo, municipio_nombre, longitud, latitud, fecha_creacion, fecha_actualizacion from public.locations_sport_leasure
+        union all
+        select nombre, actividad_tipo, municipio_codigo, municipio_nombre, longitud, latitud, fecha_creacion, fecha_actualizacion from public.locations_tourism_establishments
+    ) as combined_locations
+)
+
+select
+    nombre,
+    actividad_tipo,
+    municipio_codigo,
+    municipio_nombre,
+    longitud,
+    latitud,
+    date_part('year', year)::int as year
+from date_ranges
+order by nombre desc, municipio_codigo asc
