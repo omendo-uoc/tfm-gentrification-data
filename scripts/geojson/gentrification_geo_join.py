afbe297e7b874dd715f80eebe41e5add9c94b8ad
Omar Mendo <omendo@uoc.edu>
2024-05-28 14:36:06 +0100

    Add clustering k-means method and geojson for clustering visualization

--- scripts/geojson/gentrification_geo_join.py
+++ scripts/geojson/gentrification_geo_join.py
@@ -5,7 +5,7 @@ import pandas as pd
 geojson_path = 'data/raw/territory/municipios_desde2007_20170101.json'
 municipios_gdf = gpd.read_file(geojson_path)
 
-csv_path = 'data/processed/gentrification_processed.csv'
+csv_path = 'data/processed/gentrification_4clusters.csv'
 gentrification_df = pd.read_csv(csv_path)
 
 municipios_gdf['geocode'] = municipios_gdf['geocode'].astype(str)
@@ -16,7 +16,7 @@ gentrification_df['year'] = pd.to_datetime(gentrification_df['year'], format='%Y
 
 merged_gdf = municipios_gdf.merge(gentrification_df, left_on='geocode', right_on='munic_cp')
 
-output_geojson_path = 'data/processed/merged_gentrification_data.geojson'
+output_geojson_path = 'data/final/merged_gentrification_data_4cluster.geojson'
 merged_gdf.to_file(output_geojson_path, driver='GeoJSON')
 
 print(f'---> Merged data saved: {output_geojson_path} ✅')
