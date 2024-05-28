51a9d21a68242e44a810cfbb04b70e5fb8b3a0b7
Omar Mendo <omendo@uoc.edu>
2024-05-28 13:43:10 +0100

    Add script to join datasets in a geojson to add to Carto

---
+++ scripts/geojson/gentrification_geo_join.py
@@ -0,0 +1,22 @@
+import geopandas as gpd
+import pandas as pd
+
+
+geojson_path = 'data/raw/territory/municipios_desde2007_20170101.json'
+municipios_gdf = gpd.read_file(geojson_path)
+
+csv_path = 'data/processed/gentrification_processed.csv'
+gentrification_df = pd.read_csv(csv_path)
+
+municipios_gdf['geocode'] = municipios_gdf['geocode'].astype(str)
+gentrification_df['munic_cp'] = gentrification_df['munic_cp'].astype(str)
+
+gentrification_df['year'] = gentrification_df['year'].astype(str).str[:4]
+gentrification_df['year'] = pd.to_datetime(gentrification_df['year'], format='%Y')
+
+merged_gdf = municipios_gdf.merge(gentrification_df, left_on='geocode', right_on='munic_cp')
+
+output_geojson_path = 'data/processed/merged_gentrification_data.geojson'
+merged_gdf.to_file(output_geojson_path, driver='GeoJSON')
+
+print(f'---> Merged data saved: {output_geojson_path} ✅')
